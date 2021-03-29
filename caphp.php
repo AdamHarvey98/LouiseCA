dbcfce1b51e6add2b1da8114714ac86800d0be53
AdamHarvey98 <N00171623@student.iadt.ie>
2021-03-29 20:49:47 +0100

    No idea what I'm doing

--- caphp.php
+++ caphp.php
@@ -4,12 +4,30 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
-
-$conn = mysqli_connect($servername, $username, $password);
-
 echo $_POST['email'];
 
 echo "<br>";
 
-echo $_POST['subject'];
+$dbname = "email";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, email, subject FROM email";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "id: " . $row["id"]. " - Name: " . $row["email"]. " " . $row["subject"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
 ?>
