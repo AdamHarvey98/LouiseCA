b4b505c08e8f3559716cd65d1f771c1575332b38
AdamHarvey98 <N00171623@student.iadt.ie>
2021-03-29 21:24:23 +0100

    Sending email to database need to add in subject

--- caphp.php
+++ caphp.php
@@ -6,24 +6,40 @@ $password = "";
 
 echo $_POST['email'];
 
+echo $_POST['subject'];
+
 echo "<br>";
 
-$dbname = "catest";
+$dbname = "CAtest";
 
-// Create connection
+
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
+
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
+$sql = "INSERT INTO email (email) VALUES ('".$_POST['email']."')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+
+
+
+
 
 $sql = "SELECT id, email, subject FROM email";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-  // output data of each row
+
   while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["id"]. " - Name: " . $row["email"]. " " . $row["subject"]. "<br>";
+    echo "id: " . $row["id"]. " - Email: " . $row["email"]. " " . $row["subject"]. "<br>";
   }
 } else {
   echo "0 results";
